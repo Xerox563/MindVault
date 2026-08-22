e98af95457059e5544e32da261ac15f4d367eaff
Xerox563 <amit0101ag@gmail.com>
2026-08-23 00:40:53 +0530

    [FIX] added email-validator dependency

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/amit/Downloads/Old/Stuff/Grind/MindVault/mindvault-backend/venv
+set -gx VIRTUAL_ENV /home/amit/Downloads/Old/Stuff/Grind/MindVault/backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
